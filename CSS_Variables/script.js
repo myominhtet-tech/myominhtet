3272fef97d54cf9b433def956d4e3bb830fda2cd
Myo Min Htet <54978333+myominhtet-tech@users.noreply.github.com>
2022-05-04 23:21:24 +0630

    Add files via upload

---
+++ CSS_Variables/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('body');
+const ball = document.querySelector('.ball');
+
+body.addEvetListener('click', e => {
+    ball.style.setProperty('--pos-x', e.clientX - 80);
+    ball.style.setProperty('--pos-y', e.clientY - 80);
+
+});
+
+//set a random hex value
+
+function randomHex() {
+    let max = 1 << 24;
+    let hex = (max + Math.floor(Math.random() * max)).toString(16).slice(-6);
+    return '#${hex}';
+}
